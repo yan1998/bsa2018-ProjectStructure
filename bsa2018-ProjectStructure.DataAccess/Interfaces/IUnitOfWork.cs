93116578d8e90a462fc97c43e62c26fab01c63ec
Ян <gorshkov.yan1998@gmail.com>
2018-07-13 01:16:41 +0300

    Was implemented a unit of work

--- bsa2018-ProjectStructure.DataAccess/Interfaces/IUnitOfWork.cs
+++ bsa2018-ProjectStructure.DataAccess/Interfaces/IUnitOfWork.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using bsa2018_ProjectStructure.DataAccess.Model;
 
 namespace bsa2018_ProjectStructure.DataAccess.Interfaces
 {
     public interface IUnitOfWork
     {
+        IRepository<Aircraft> Aircrafts { get; }
+        IRepository<AircraftType> AircraftTypes { get; }
+        IRepository<Departure> Departures { get; }
+        IRepository<Flight> Flights { get; }
+        IRepository<Pilot> Pilots { get; }
+        IRepository<Stewardess> Stewardess { get; }
+        IRepository<Ticket> Tickets { get; }
+        IRepository<Crew> Crews { get; }
+
         void SaveChages();
         Task SaveChangesAsync();
     }
---
+++ bsa2018-ProjectStructure.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Threading.Tasks;
+using bsa2018_ProjectStructure.DataAccess.Interfaces;
+using bsa2018_ProjectStructure.DataAccess.Model;
+
+namespace bsa2018_ProjectStructure.DataAccess.Repository
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        private DataContext context;
+        private AircraftRepository aircraftRepository;
+        private AircraftTypesRepository aircraftTypesRepository;
+        private CrewsRepository crewsRepository;
+        private DeparturesRepository departuresRepository;
+        private FlightsRepository flightsRepository;
+        private PilotsRepository pilotsRepository;
+        private StewardessRepository stewardessRepository;
+        private TicketsRepository ticketsRepository;
+
+        public UnitOfWork(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public IRepository<Aircraft> Aircrafts
+        {
+            get
+            {
+                if (aircraftRepository == null)
+                    aircraftRepository = new AircraftRepository(context);
+                return aircraftRepository;
+            }
+        }
+
+        public IRepository<AircraftType> AircraftTypes
+        {
+            get
+            {
+                if (aircraftTypesRepository == null)
+                    aircraftTypesRepository = new AircraftTypesRepository(context);
+                return aircraftTypesRepository;
+            }
+        }
+
+        public IRepository<Departure> Departures
+        {
+            get
+            {
+                if (departuresRepository == null)
+                    departuresRepository = new DeparturesRepository(context);
+                return departuresRepository;
+            }
+        }
+
+        public IRepository<Flight> Flights
+        {
+            get
+            {
+                if (flightsRepository == null)
+                    flightsRepository = new FlightsRepository(context);
+                return flightsRepository;
+            }
+        }
+
+        public IRepository<Pilot> Pilots
+        {
+            get
+            {
+                if (pilotsRepository == null)
+                    pilotsRepository = new PilotsRepository(context);
+                return pilotsRepository;
+            }
+        }
+
+        public IRepository<Stewardess> Stewardess
+        {
+            get
+            {
+                if (stewardessRepository == null)
+                    stewardessRepository = new StewardessRepository(context);
+                return stewardessRepository;
+            }
+        }
+
+        public IRepository<Ticket> Tickets
+        {
+            get
+            {
+                if (ticketsRepository == null)
+                    ticketsRepository = new TicketsRepository(context);
+                return ticketsRepository;
+            }
+        }
+
+        public IRepository<Crew> Crews
+        {
+            get
+            {
+                if (crewsRepository == null)
+                    crewsRepository = new CrewsRepository(context);
+                return crewsRepository;
+            }
+        }
+
+        public void SaveChages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
